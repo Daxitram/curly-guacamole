6b7404059e2a73d6402bb948daf268f7eab8684e
Daxitram <37157568+Daxitram@users.noreply.github.com>
2023-06-07 17:39:00 +0200

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
   </head>
   <body>
     <h1>Hello, world!</h1>
+    <img src="_2142e5af-99f5-4eee-974d-2939815ac08b.jpg">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
   </body>
 </html>
